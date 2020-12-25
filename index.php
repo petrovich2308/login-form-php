b349b5b1661f976684358d0a8cb1f006c512afb2
petrovich2308 <osgard.avs.dev@gmail.com>
2020-12-25 08:56:24 +0200

    creating a form handler file

--- index.php
+++ index.php
@@ -10,8 +10,11 @@
 </head>
 <body>
 
-<form>
+<form action="login.php" method="post">
     <h2>LOGIN</h2>
+    <?php if (isset($_GET['error'])) { ?>
+        <p class="error"><?php echo $_GET['error'];?></p>
+    <?php } ?>
     <label for="uname">User Name</label>
     <input type="text" id="uname" name="uname"> <br>
 
